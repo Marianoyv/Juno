a3f01061d833bd53acf1a34f291c1ead584e8719
marianoyv <mariano13age@gmail.com>
2022-12-01 01:03:05 -0300

    fn

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
-import Vue from "vue";
-import App from "./App.vue";
+const text = document.querySelector(".changer");
 
-Vue.config.productionTip = false;
-
-new Vue({
-  render: h => h(App)
-}).$mount("#app");
-
+    const textLoad = () => {
+        setTimeout(() => {
+            text.textContent = "Compro Cobre";
+        }, 0);
+        setTimeout(() => {
+            text.textContent = "Compro Bronce";
+        }, 2000);
+        setTimeout(() => {
+            text.textContent = "Compro Plomo";
+        }, 4000);
+        setTimeout(() => {
+            text.textContent = "Compro Aluminio";
+        }, 6000);
+        setTimeout(() => {
+            text.textContent = "Compro Perfil";
+        }, 8000);
+        setTimeout(() => {
+            text.textContent = "Compro Acero";
+        }, 10000);
+        setTimeout(() => {
+            text.textContent = "Compro Carter";
+        }, 12000);
+        setTimeout(() => {
+            text.textContent = "Compro Radiadores";
+        }, 14000);
+        setTimeout(() => {
+            text.textContent = "Compro Cables";
+        }, 16000); //1s = 1000 milliseconds
+    }
+    textLoad();
+    setInterval(textLoad, 12000);
